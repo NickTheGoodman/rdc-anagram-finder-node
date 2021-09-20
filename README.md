2eada129fe456f8b46cd5fa862c7ec40b43ee989
NickTheGoodman <nick.thegoodman@gmail.com>
2021-09-20 18:39:37 -0500

    Final cleanup of BruteForceAnagramSolver

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-## Anagram Finder
+# Anagram Finder
+
+Note: this repo was written in WSL on Windows using Ubuntu 20.04 LTS image.
 
 ## Time Tracking
 - Day 1 [4 hrs]
@@ -8,10 +10,72 @@
     - Setup initial repo skeleton 
 - Day 2 [4 hrs]
     - Implement dictionary loading and input processing in BruteForceAnagramSolver class
-- Day 3 [4 hrs]
-    - Implement BruteForceAnagramSolver (almost entirely)
-- Day 4 []
-    - -
+- Day 3 [6 hrs]
+    - Finish implementation of BruteForceAnagramSolver
+    - Use a Set for storing anagram possibilities to remove dupes
+    - Handle whitespace in input
+    - Write README instructions
+    - Cleanup output
   
 ## Notes and Assumptions About Anagrams
 - Anagrams are case-insensitive. For example, "Cinema" is an anagram of "iceman".
+- If a user provides a combination of letters that is not actually a word in the provided dictionary,
+  the AnagramSolver won't care; it will still look for anagrams of those letters.
+  
+## Build and Execution Instructions
+
+- Ensure you have node installed at least version 16.
+- Run a local npm install:
+```shell
+$ npm install
+```
+- Transpile the typescript source into executable javascript:
+```shell
+$ npm run transpile
+
+> rdc-anagram-finder-node@1.0.0 transpile
+> tsc
+
+```
+- Run the app, passing in a single positional argument: a path to a dictionary .txt file.
+```shell
+$ npm start ./dictionary-data/dictionary.txt
+
+> rdc-anagram-finder-node@1.0.0 start
+> node dist/src/app.js "./dictionary-data/dictionary.txt"
+
+Welcome to the Anagram Finder v1: Brute Force
+---------------------------------------------
+Dictionary loaded in 24 ms
+Enter a word. > 
+```
+- When prompted, either provide a single word or "exit" to kill the app.
+  Phrases will be rejected, but leading/trailing whitespace are ok.
+```shell
+Enter a word. > post
+Finding anagrams for: post
+4 anagrams found for post in 9 ms
+post,spot,stop,tops
+
+Enter another word. > ice man
+Bad input.
+
+Enter another word. >     iceman
+Finding anagrams for: iceman
+3 anagrams found for iceman in 330 ms
+anemic,cinema,iceman
+
+Enter another word. > exit
+Exiting.
+```
+- Note: since "exit" serves as an input command to exit the app,
+  in order to run the AnagramSolver on the actual word "exit",
+  simply provide an anagram of "exit" such as "tixe". The anagram you provide does not have
+  to be in the dictionary to trigger the AnagramSolver.
+```shell
+Enter another word. > tixe
+Finding anagrams for: tixe
+1 Anagrams found for tixe in 9ms
+exit
+
+```
--- src/brute-force-anagram-solver.ts
+++ src/brute-force-anagram-solver.ts
@@ -16,7 +16,7 @@ export class BruteForceAnagramSolver {
 
         const loadEnd = Date.now();
         const loadDuration = loadEnd - loadStart;
-        console.log(`Dictionary loaded in ${loadDuration} ms`);
+        console.log(`Dictionary loaded in ${loadDuration} ms\n`);
     }
 
     public processInput() {
@@ -25,7 +25,7 @@ export class BruteForceAnagramSolver {
             output: process.stdout,
         });
 
-        const wordPattern = /^[a-zA-Z]+$/s;
+        const wordPattern = /^\s*[a-zA-Z]+\s*$/s;
         const getInput = (input: string) => {
             if (input === "exit") {
                 ui.close();
@@ -33,8 +33,9 @@ export class BruteForceAnagramSolver {
             }
 
             if (!wordPattern.test(input)) {
-                console.log("Bad input.")
+                console.log("Bad input.\n")
             } else {
+                input = input.trim();
                 console.log(`Finding anagrams for: ${input}`);
                 ui.emit('find_anagram', input);
             }
@@ -42,11 +43,12 @@ export class BruteForceAnagramSolver {
         };
         ui.question('Enter a word. > ', getInput);
 
-        ui.on('find_anagram', (input: string) => this._findAnagrams(input))
-          .on("close", () => {
+        ui.on('find_anagram', (input: string) => {
+            this._findAnagrams(input);
+        }).on("close", () => {
             console.log("Exiting.");
             process.exit(0);
-          });
+        });
     }
 
     private _readDictionaryFile(filename: string) {
@@ -68,24 +70,28 @@ export class BruteForceAnagramSolver {
         const findStart = Date.now();
 
         const possibleAnagrams = this._generatePossibleAnagrams(word);
-        console.log(`DEBUG: possibleAnagrams:`);
-        console.log(possibleAnagrams);
+        // console.log(`DEBUG: ${possibleAnagrams.size} possible anagrams found`);
         const foundAnagrams = this._findAnagramsInDictionary(possibleAnagrams);
+        foundAnagrams.sort();
 
         const findEnd = Date.now();
         const findDuration = findEnd - findStart;
 
-        const numAnagramsOutput = foundAnagrams.length === 0 ? "No" : foundAnagrams.length;
-        console.log(`${numAnagramsOutput} Anagrams found for ${word} in ${findDuration}ms`);
-        console.log(foundAnagrams.toString());
+        const numAnagrams = foundAnagrams.length;
+        const output = numAnagrams === 0 ?
+            `No anagrams found for ${word} in ${findDuration} ms` :
+            numAnagrams === 1 ?
+            `${numAnagrams} anagram found for ${word} in ${findDuration} ms\n${foundAnagrams.toString()}\n` :
+            `${numAnagrams} anagrams found for ${word} in ${findDuration} ms\n${foundAnagrams.toString()}\n`;
+        console.log(output);
     }
 
     private _generatePossibleAnagrams(word: string) {
-        const loadStart = Date.now();
+        // const loadStart = Date.now();
 
-        const possibleAnagrams: string[] = [];
+        const possibleAnagrams = new Set<string>();
         if (word.length === 1) {
-            possibleAnagrams.push(word);
+            possibleAnagrams.add(word);
         }
         const tupleStack: [string, string][] = [];
         tupleStack.push(["", word]);
@@ -103,24 +109,24 @@ export class BruteForceAnagramSolver {
                 const charsSoFar = prevChars + middleChar;
                 const nextChars = leftChars + rightChars;
                 if (nextChars.length === 1) {
-                    possibleAnagrams.push(charsSoFar + nextChars);
+                    possibleAnagrams.add(charsSoFar + nextChars);
                 } else {
                     tupleStack.push([charsSoFar, nextChars]);
                 }
             }
         }
 
-        const loadEnd = Date.now();
-        const loadDuration = loadEnd - loadStart;
-        console.log(`Anagram possibilities generated in ${loadDuration} ms`);
+        // const loadEnd = Date.now();
+        // const loadDuration = loadEnd - loadStart;
+        // console.log(`DEBUG: Anagram possibilities generated in ${loadDuration} ms`);
         return possibleAnagrams;
     }
 
-    private _findAnagramsInDictionary(possibleAnagrams: string[]) {
+    private _findAnagramsInDictionary(possibleAnagrams: Set<string>) {
         const foundAnagrams: string[] = [];
-        possibleAnagrams.map((possibleAnagram) => {
-            const anagramFound = this._wordList.indexOf(possibleAnagram) != -1;
-            if (anagramFound) {
+
+        possibleAnagrams.forEach((possibleAnagram) => {
+            if (this._wordList.indexOf(possibleAnagram) != -1) {
                 foundAnagrams.push(possibleAnagram);
             }
         });
